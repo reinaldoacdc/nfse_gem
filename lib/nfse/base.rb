4e014dd431f14b94db52be374ba31d866ad982f4
guilhobel <guiclaudior@gmail.com>
2024-02-21 10:43:08 -0300

    alterar url da nota

--- lib/nfse/base.rb
+++ lib/nfse/base.rb
@@ -8,7 +8,7 @@ module Nfse
             if Rails.env.production?
                 {
                     '3131703' => 'http://servicosweb.itabira.mg.gov.br:90/nfse.portal.integracao/services.svc?Wsdl',                
-                    '3106200' => 'https://bhissdigital.pbh.gov.br/bhiss-ws/nfse?wsdl'
+                    '3106200' => 'https://bhissdigitalws.pbh.gov.br/bhiss-ws/nfse?wsdl'
                 }
             else
                 {
